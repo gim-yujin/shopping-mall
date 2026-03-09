62ead9937588afa380147221dbedfc2ffc234dd5
gim-yujin <gim.yujin.dev@gmail.com>
2026-03-09 12:56:28 +0900

    remove unused import

--- src/test/java/com/shop/domain/cart/controller/api/CartApiControllerUnitTest.java
+++ src/test/java/com/shop/domain/cart/controller/api/CartApiControllerUnitTest.java
@@ -25,7 +25,6 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
